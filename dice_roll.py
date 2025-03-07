15f6a4162fcc6af2075346b8b5fa2377475cfebd
Joshua Ferreira <kointaker@Joshuas-MacBook-Air.local>
2025-03-07 12:44:45 -0500

    Basic funcionality

---
+++ dice_roll.py
@@ -0,0 +1,13 @@
+# Python file
+
+import random
+import time
+
+x = input("Spin? 1 = Yes 2 = No: ")
+
+if x == 1:
+    print("Rolling dice...")
+    time.sleep(1)
+
+if x == 2:
+    print("Goodbye")
