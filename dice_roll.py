eb5745dbb8c53ec89f68435cbcad9b428dc4c187
Joshua Ferreira <kointaker@Joshuas-MacBook-Air.local>
2025-03-07 12:48:02 -0500

    Small Changes

--- dice_roll.py
+++ dice_roll.py
@@ -1,13 +1,14 @@
-# Python file
+# Dice roll app
 
 import random
 import time
 
-x = input("Spin? 1 = Yes 2 = No: ")
+x = int(input("Spin? 1 = Yes 2 = No: "))
 
 if x == 1:
     print("Rolling dice...")
     time.sleep(1)
+
 
 if x == 2:
     print("Goodbye")
